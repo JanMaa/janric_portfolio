63a8194139d5ee97bb517829b41e665c934eabc2
JanMaa <janricmalate@gmial.com>
2023-10-21 05:56:51 -0400

    added added meta data in base

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #Static folder (settings.py): 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = [ BASE_DIR / 'static' ] 
 # STATIC_ROOT = 'static_root'
 
