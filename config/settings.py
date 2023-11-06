a1952fae1ef66e99c17070b3ce6f09468b29d722
JanMaa <janricmalate@gmial.com>
2023-11-06 00:40:50 -0500

    added comments on settings

--- config/settings.py
+++ config/settings.py
@@ -13,11 +13,11 @@ import os
 from pathlib import Path
 from dotenv import dotenv_values
 
-config = dotenv_values(".env")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# load env variables
+config = dotenv_values(".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
